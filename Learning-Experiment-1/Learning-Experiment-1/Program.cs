435ac5274619d855a595d1c665037f63520df25d
Mitchell Herbert <supermitchell2@gmail.com>
2019-11-12 18:17:09 -0800

    Modified to print "test"

---
+++ Learning-Experiment-1/Learning-Experiment-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Learning_Experiment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+        }
+    }
+}
